f694f66efb5d57f1c9d5f014b55c554dd59d7fc6
NoratheeDear <noratee5@hotmail.com>
2024-07-22 11:14:23 +0700

    create_lego_riff_note_combi done :>

--- lego_riff_01.py
+++ lego_riff_01.py
@@ -4,10 +4,11 @@
 
 
 # NEXT:
-# create_midi_lego_riff_1file -> package the functions that I have with 3 inputs noteblock,note_lengh,n_repeat, bpm
-# create_midi_lego_riff_combi -> build on top of create_midi_lego_riff_1file that will create midi files with different scales, keys, bpm in a loop with ost
+# 1)create_midi_lego_riff_combi -> 
+    # try to use notes_df(output) from create_lego_riff_note_combi
+        # build on top of create_midi_lego_riff_1file that will create midi files with different scales, keys, bpm in a loop with ost
 
-# convert midi directly to mp3(not important bc I can use format factory for now)
+# 2)convert midi directly to mp3(not important bc I can use format factory for now)
 # try to do that manually(Clade Music_with_Code_03)
 
 from music21 import stream
@@ -21,6 +22,7 @@ from functools import partial
 from mingus.containers import Note
 import numpy as np
 import inspect_py as inp
+import pandas as pd
 # notes.int_to_note()
 # scales._Scale
 # based on migus 0.6.1
@@ -28,11 +30,133 @@ import inspect_py as inp
 ScaleType = Union[scales._Scale,Literal["Major","Minor","Natural minor","Ionian","Dorian","Phrygian","Lydian","Mixolydian","Aeolian","Locrian","Harmonic minor","Melodic minor","Whole tone","Chromatic"]]
 OUTPUT_FOLDER = Path(r"C:\Users\Heng2020\OneDrive\D_Code\Python\Python Music\2024\01 Lego Riff Creation\lego_riff_creation\test_output")
 
+def create_lego_riff_note_combi(
+    # out_prefixname:Union[Path,str]
+    lego_block_num: List[int]
+    # ,note_lengths: List[int]
+    ,directions: Union[List[str],Literal["up","down"]] = ["up","down"]
+    # ,bpms: Union[int,List[int]] = 120
+    ,n: Union[int,  Dict[Literal["up","down"],int]  ] = 7
+    ,key_s: Union[str,List[str]] = ["C", "D", "E", "F", "G", "A", "B", "C#", "Eb", "F#", "Ab", "Bb"]
+    ,scale_types: Union[ScaleType, List[ScaleType]] = ["Major","Natural minor","Dorian","Phrygian","Lydian","Mixolydian","Locrian"]
+    ,root_degree: Union[Literal["max","min"],int] = "max"
+    ,octaves: Union[int,List[int]] = 3
+    ,longer_last_note:Union[bool,int] = 1
+    ) -> pd.DataFrame:
+    # took about 1 hr(including testing)
+    import pandas as pd
+    """
+    signature function
+
+    build on top of create_lego_riff_note to loop through different parameters and create a combination of midi files
+    n can be int or dict  with key "up" and "down" eg n = {"up":6,"down":7}
+
+    output pd.DataFrame {'direction','scale_type','key','octave','notes'}
+
+    """
+    # has error with D# as key Gb
+
+    directions_in = list(directions) if isinstance(directions,list) else [directions]
+    scale_types_in = list(scale_types) if isinstance(scale_types,list) else [scale_types]
+    key_s_in = list(key_s) if isinstance(key_s,list) else [key_s]
+
+    # has error with D# as key Gb, so I want to support D#, Gb as input
+    # for other key like Db, G#, A# I would replace them(not to sure that it would throw an error but just replace for safety)
+    REPLACE_KEY_DICT = {
+        'D#': 'Eb',
+        'Gb': 'F#',
+        'Db': 'C#',
+        'G#': 'Ab',
+        'A#': 'Bb'
+    }
+
+    for i, key in enumerate(key_s_in):
+        if key in REPLACE_KEY_DICT.keys():
+            key_s_in[i] = REPLACE_KEY_DICT[key]
+
+
+    octaves_in = list(octaves) if isinstance(octaves,list) else [octaves]
+
+    out_df = pd.DataFrame(columns = ["direction","scale_type","key","octave","notes"])
+    results = []
+
+    for direction in directions_in:
+        for scale_type in scale_types_in:
+            for key in key_s_in:
+                for octave in octaves_in:
+                    if isinstance(n,int):
+                        curr_riff_notes = create_lego_riff_note(
+                                            lego_block_num = lego_block_num
+                                            ,direction = direction
+                                            ,n = n
+                                            ,key = key
+                                            ,scale_type = scale_type
+                                            ,root_degree = root_degree
+                                            ,octave = octave
+                                            )
+                    elif isinstance(n,dict):
+                        curr_riff_notes = create_lego_riff_note(
+                                            lego_block_num = lego_block_num
+                                            ,direction = direction
+                                            ,n = n[direction]
+                                            ,key = key
+                                            ,scale_type = scale_type
+                                            ,root_degree = root_degree
+                                            ,octave = octave
+                                            )
+                    curr_dict = {
+                        "direction": direction,
+                        "scale_type": scale_type,
+                        "key": key,
+                        "octave": octave,
+                        "notes": curr_riff_notes
+                        }
+                    results.append(curr_dict)
+
+    out_df = pd.DataFrame(results)
+    return out_df
+     
+
+
+def create_midi_lego_riff_combi(
+    out_prefixname:Union[Path,str]
+    ,lego_block_num: List[int]
+    ,note_lengths: List[int]
+    ,directions: Union[List[str],Literal["up","down"]] = ["up","down"]
+    ,bpms: Union[int,List[int]] = 120
+    ,n: Union[int,  Dict[Literal["up","down"],int]  ] = 7
+    ,key_s: Union[str,List[str]] = ["C", "D", "E", "F", "G", "A", "B", "C#", "Eb", "F#", "Ab", "Bb"]
+    ,scale_types: Union[ScaleType, List[ScaleType]] = ["Major","Natural minor","Dorian","Phrygian","Lydian","Mixolydian","Locrian"]
+    ,root_degree: Union[Literal["max","min"],int] = "max"
+    ,octaves: Union[int,List[int]] = 3
+    ,longer_last_note:Union[bool,int] = 1
+    ) -> None:
+
+    # has error with D# as key Gb
+
+    """
+    out_prefixname is without ".mid"
+    signature function
+
+    build on top of create_midi_lego_riff_1file to loop through different parameters and create a combination of midi files
+    n can be int or dict  with key "up" and "down" eg n = {"up":6,"down":7}
+
+    """
+    import os_toolkit as ost
+    # convert single value all into list to support both
+    directions_in = list(directions) if isinstance(directions,list) else [directions]
+    scale_types_in = list(scale_types) if isinstance(scale_types,list) else [scale_types]
+    key_s_in = list(key_s) if isinstance(key_s,list) else [key_s]
+    octaves_in = list(octaves) if isinstance(octaves,list) else [octaves]
+    bpms_in = list(bpms) if isinstance(directions,list) else [bpms]
+
+    pass
 def create_midi(out_filename: Union[Path, str], 
                 note_names: List[str], 
                 note_lengths: Union[None, List[float]] = None,
                 bpm: float = 120) -> None:
     from music21 import stream, note, midi, tempo
+    # high tested
     """
     Create a MIDI file with the specified note names, lengths, and tempo, then save it to the given filename.
     
@@ -76,6 +200,7 @@ def create_midi_repeate_tempo(
         bpm:float = 120,
         longer_last_note:Union[int,bool] =1,
         ) -> None:
+    # high tested
     """
     do similar thing to create_midi but note_lengths is smarter because it would take only the block of note_lengths, and assume to have the same tempo the whole time
     longer_last_note will extend the lastnote for a bit
@@ -157,53 +282,53 @@ def create_midi_repeate_tempo(
     
 #     return transposed_notes
 
-def convert_scale_degrees(
-    scale_degrees: List[int],
-    from_key: str,
-    from_scale_type: ScaleType,
-    to_key: str,
-    to_scale_type: ScaleType,
-    octave: int = 4
-) -> List[str]:
-    def get_scale(key: str, scale_type: ScaleType) -> scales._Scale:
-        if isinstance(scale_type, scales.Scale):
-            return scale_type
-        elif isinstance(scale_type, type) and issubclass(scale_type, scales.Scale):
-            return scale_type(key)
-        elif isinstance(scale_type, str):
-            scale_type = scale_type.lower()
-            if scale_type == "major":
-                return scales.Major(key)
-            elif scale_type in ["minor", "natural minor"]:
-                return scales.NaturalMinor(key)
-            else:
-                raise ValueError(f"Unsupported scale type string: {scale_type}")
-        else:
-            raise TypeError(f"Unsupported scale type: {type(scale_type)}")
+# def convert_scale_degrees(
+#     scale_degrees: List[int],
+#     from_key: str,
+#     from_scale_type: ScaleType,
+#     to_key: str,
+#     to_scale_type: ScaleType,
+#     octave: int = 3
+# ) -> List[str]:
+#     def get_scale(key: str, scale_type: ScaleType) -> scales._Scale:
+#         if isinstance(scale_type, scales.Scale):
+#             return scale_type
+#         elif isinstance(scale_type, type) and issubclass(scale_type, scales.Scale):
+#             return scale_type(key)
+#         elif isinstance(scale_type, str):
+#             scale_type = scale_type.lower()
+#             if scale_type == "major":
+#                 return scales.Major(key)
+#             elif scale_type in ["minor", "natural minor"]:
+#                 return scales.NaturalMinor(key)
+#             else:
+#                 raise ValueError(f"Unsupported scale type string: {scale_type}")
+#         else:
+#             raise TypeError(f"Unsupported scale type: {type(scale_type)}")
 
-    from_scale = get_scale(from_key, from_scale_type)
-    to_scale = get_scale(to_key, to_scale_type)
+#     from_scale = get_scale(from_key, from_scale_type)
+#     to_scale = get_scale(to_key, to_scale_type)
     
-    # Get the notes for the given scale degrees in the original scale
-    original_notes = [Note(from_scale.get_notes(degree - 1), octave) for degree in scale_degrees]
+#     # Get the notes for the given scale degrees in the original scale
+#     original_notes = [Note(from_scale.get_notes(degree - 1), octave) for degree in scale_degrees]
     
-    # Adjust octaves for notes above the reference octave
-    for i, note in enumerate(original_notes):
-        if i > 0 and note.name < original_notes[i-1].name:
-            note.octave += 1
+#     # Adjust octaves for notes above the reference octave
+#     for i, note in enumerate(original_notes):
+#         if i > 0 and note.name < original_notes[i-1].name:
+#             note.octave += 1
 
-    # Find the interval between the two keys
-    interval = intervals.determine(from_key, to_key)
+#     # Find the interval between the two keys
+#     interval = intervals.determine(from_key, to_key)
     
-    # Transpose each note
-    # transposed_notes is the Note object
-    transposed_notes = [Note(notes.augment(note.name, interval), note.octave) for note in original_notes]
-    transposed_notes_str = [f"{notes.augment(note.name, interval)}{note.octave}" for note in original_notes]
+#     # Transpose each note
+#     # transposed_notes is the Note object
+#     transposed_notes = [Note(notes.augment(note.name, interval), note.octave) for note in original_notes]
+#     transposed_notes_str = [f"{notes.augment(note.name, interval)}{note.octave}" for note in original_notes]
     
-    # Convert transposed notes to scale degrees in the new scale
-    new_scale_degrees = [to_scale.determine(note.name)[0] + 1 for note in transposed_notes]
+#     # Convert transposed notes to scale degrees in the new scale
+#     new_scale_degrees = [to_scale.determine(note.name)[0] + 1 for note in transposed_notes]
     
-    return transposed_notes_str
+#     return transposed_notes_str
 
 def _get_scale(key: str, scale_type: ScaleType) -> scales._Scale:
         # Helper function to get the appropriate scale object
@@ -356,18 +481,38 @@ def create_lego_riff_note(
         ,key:str = "C"
         ,scale_type:ScaleType = "Major"
         ,root_degree:Union[Literal["max","min"],int] = "max"
+        ,octave:int = 3
         ,out_as_str:bool = True
         ) -> Union[List[Note],List[str]]:
+    # medium tested
+    """
+    Generate a musical riff based on a sequence of Lego block numbers.
+
+    Args:
+        lego_block_num (List[int]): A list of Lego block numbers.
+        direction (Literal["up","down"]): The direction of the riff. "up" for ascending and "down" for descending.
+        n (int, optional): The number of notes in the riff. Defaults to 7.
+        key (str, optional): The key of the riff. Defaults to "C".
+        scale_type (ScaleType, optional): The type of scale to use. Defaults to "Major".
+        root_degree (Union[Literal["max","min"],int], optional): The root degree of the scale. Defaults to "max".
+        out_as_str (bool, optional): Whether to output the riff as a list of strings or a list of Note objects. Defaults to True.
+
+    Returns:
+        Union[List[Note],List[str]]: The generated riff as a list of Note objects or a list of strings.
+
+    Raises:
+        ValueError: If the direction is neither "up" nor "down".
+    """
     
     if direction.lower() in ["up"]:
         scale_degrees = make_num_seq(lego_block_num, n=n, increment=1)
     elif direction.lower() in ["down"]:
-        scale_degrees = make_num_degree_down(lego_block_num, n=n, increment=1,root_degree=root_degree)
+        scale_degrees = make_num_degree_down(lego_block_num, n=n, increment=-1,root_degree=root_degree)
     else:
         raise ValueError("direction must be either 'up' or 'down'")
     
-    riff_notes_obj = convert_num_to_scale(scale_degrees,scale_type=scale_type,key=key,out_as_str=False)
-    riff_notes_str = convert_num_to_scale(scale_degrees,scale_type=scale_type,key=key,out_as_str=True)
+    riff_notes_obj = convert_num_to_scale(scale_degrees,scale_type=scale_type,key=key,out_as_str=False,octave=octave)
+    riff_notes_str = convert_num_to_scale(scale_degrees,scale_type=scale_type,key=key,out_as_str=True,octave=octave)
 
     if out_as_str:
         return riff_notes_str
@@ -384,11 +529,16 @@ def create_midi_lego_riff_1file(
     ,key:str = "C"
     ,scale_type:ScaleType = "Major"
     ,root_degree:Union[Literal["max","min"],int] = "max"
-    ,longer_last_note:Union[bool,int] = False
+    ,longer_last_note:Union[bool,int] = 1
     ) -> None:
+
+    """
+    signature function
+    """
+
+    # medium tested
     riff_notes = create_lego_riff_note(
         lego_block_num = lego_block_num
-        ,note_lengths = note_lengths
         ,direction = direction
         ,n = n
         ,key = key
@@ -402,6 +552,35 @@ def create_midi_lego_riff_1file(
 
 ####################################
 
+def test_create_lego_riff_note_combi():
+    input_test_list = [None]*100
+    input_test_list[0] = {
+        "out_filename": OUTPUT_FOLDER / "Dawn_create_1_file_240bpm_v01.mid"
+        ,"lego_block_num": [2,3,2,1,2]
+        ,"note_lengths": [1,0.5,0.5,1,1]
+        ,"directions": "up"
+        ,"bpm": [240,200]
+        ,"n": 7
+        ,"root_degree": "max"
+        ,"longer_last_note": 1
+        
+    }
+
+    input_test_list[1] = {
+        "out_filename": OUTPUT_FOLDER / "Dusk_create_1_file_200bpm_v01.mid"
+        ,"lego_block_num": [3,2,1]
+        ,"note_lengths":  [0.75,0.25,1]
+        ,"bpm": 200
+        ,"n": 6
+        ,"root_degree": 2
+        ,"longer_last_note": 1
+        
+    }
+    for info_dict in input_test_list:
+        if info_dict:
+            riff_info_only = {key: info_dict[key] for key in info_dict.keys() if key not in ["out_filename","note_lengths","bpm","longer_last_note"] }
+            riff_notes = create_lego_riff_note_combi(**riff_info_only)
+            
 
 def test_midi_to_audio():
     import py_string_tool as pst
@@ -464,6 +643,41 @@ def test_make_num_degree_down():
     assert actual03 == expect03, inp.assert_message(actual03,expect03)
     assert np.array_equal(actual04 ,expect04), inp.assert_message(actual04, expect04)
     
+def test_create_midi_lego_riff_1file():
+    input_test_list = [None]*100
+    input_test_list[0] = {
+        "out_filename": OUTPUT_FOLDER / "Dawn_create_1_file_240bpm_v01.mid"
+        ,"lego_block_num": [2,3,2,1,2]
+        ,"note_lengths": [1,0.5,0.5,1,1]
+        ,"direction": "up"
+        ,"bpm": 240
+        ,"n": 7
+        ,"key": "C"
+        ,"scale_type": "Major"
+        ,"root_degree": "max"
+        ,"longer_last_note": 1
+        
+    }
+
+    input_test_list[1] = {
+        "out_filename": OUTPUT_FOLDER / "Dusk_create_1_file_200bpm_v01.mid"
+        ,"lego_block_num": [3,2,1]
+        ,"note_lengths":  [0.75,0.25,1]
+        ,"direction": "down"
+        ,"bpm": 200
+        ,"n": 6
+        ,"key": "C"
+        ,"scale_type": "Minor"
+        ,"root_degree": 2
+        ,"longer_last_note": 1
+        
+    }
+    for info_dict in input_test_list:
+        if info_dict:
+            riff_info_only = {key: info_dict[key] for key in info_dict.keys() if key not in ["out_filename","note_lengths","bpm","longer_last_note"] }
+            riff_notes = create_lego_riff_note(**riff_info_only)
+            create_midi_lego_riff_1file(**info_dict)
+
 
 ####################################
 
@@ -513,14 +727,16 @@ def main_real_test():
     # create_block01()
 
 def main_test():
+    test_create_lego_riff_note_combi()
+    test_create_midi_lego_riff_1file()
     test_make_num_degree_down()
     # test_convert_num_to_scale()
     # test_make_num_seq()
     # test_midi_to_audio()
 
 def main():
-    main_real_test()
     main_test()
+    main_real_test()
     
 if __name__ == '__main__':
     main()
